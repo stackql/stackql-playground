781ed47610b8921ec476e585fe9d9b319f9ca210
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-09-29 22:37:10 +1000

    added tailwind to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-3xl font-bold underline">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
