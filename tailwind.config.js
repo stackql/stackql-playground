b04000d08b7e3bf560727aa94a5b6f64b475ef42
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-02 15:38:31 +1100

    finished initial sketch of dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Open Sans"'],
+      },
+    }
   },
   plugins: [],
 }
