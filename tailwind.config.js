05aa77e41796ebcbc60635d2f5347366e1292d91
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-04 21:16:19 +1100

    quick style fix
    - changed the button style to tailwind
    - backdrop modal to full screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors:{
+        primary: '#0F4C81'
+      },
       fontFamily: {
         'sans': ['"Open Sans"'],
       },
