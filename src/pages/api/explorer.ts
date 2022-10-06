b852b5fdb6070e1582da6ee63af97dfe4316326b
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-07 09:49:26 +1100

    able to fetch explorer view from middleware

--- src/pages/api/explorer.ts
+++ src/pages/api/explorer.ts
@@ -19,12 +19,12 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const url = localMiddlewareUrl;
-  ///{"data":[{"name":"github","version":"v0.3.6"}],"metadata":null}
-  const providerResponse = await fetch(providersUrl(url));
-  const providerData = (await providerResponse.json()) as { data: Provider[] };
-  const entityTreePromises = providerData.data.map(async (provider) => {
+  const providers = await getDataFromResponse<Provider>(providersUrl(url));
+
+  const entityTreePromises = providers.map(async (provider, index) => {
     const servicesUrl = getServiceUrl(url, provider.name);
     const services = await getDataFromResponse<Service>(servicesUrl);
+
     const serviceWithResource: any[] = [];
     for (const service of services) {
       const resourceUrl = getResourceUrl(url, provider.name, service.name);
@@ -32,15 +32,18 @@ export default async function handler(
       serviceWithResource.push({
         ...service,
         level: 1,
-        resources: resources.map((resource) => ({ ...resource, level: 2 })),
+        children: resources.map((resource) => ({ ...resource, level: 2 })),
       });
     }
-    return {
+    const providerWithService = {
       ...provider,
       level: 0,
-      service: serviceWithResource,
+      children: serviceWithResource,
+      id: index.toString(),
     };
+    return providerWithService;
   });
+
   const entityTree = await Promise.all(entityTreePromises);
   res.status(200).json(entityTree);
 }
