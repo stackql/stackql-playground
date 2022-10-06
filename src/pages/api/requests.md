9a55a93bff1d035896272ee00f86badbd35082d4
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-07 00:50:00 +1100

    created api fetches resource tree from provider

---
+++ src/pages/api/requests.md
@@ -0,0 +1,5 @@
+### Explorer
+```
+curl --location --request GET 'http://localhost:3000/api/explorer'
+
+```
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface Provider {
+  name: string;
+  version: string;
+}
+
+export interface Service {
+  id: string;
+  name: string;
+  title: string;
+  version: string;
+}
+export interface Resource {
+  id: string;
+  name: string;
+}
