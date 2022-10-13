754e838c64df72d8bbeccdece40023e4457c6202
jeffreyaven <javen@avensolutions.com>
2022-10-13 18:39:38 +1100

    nextjs env vars

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM node:16.17.0-bullseye-slim
 RUN apt-get update && apt-get install -y --no-install-recommends dumb-init
 ENV NODE_ENV production
-ENV MIDDLEWARE_SCHEME http
-ENV MIDDLEWARE_HOST localhost
-ENV MIDDLEWARE_PORT 8080
 WORKDIR /app
 RUN adduser --system --uid 1001 nextjs
 RUN addgroup --system --gid 1001 nextjs
--- next.config.js
+++ next.config.js
@@ -8,10 +8,12 @@ const nextConfig = {
 const removeImports = require("next-remove-imports")();
 module.exports = removeImports({
   serverRuntimeConfig: {
-    middlewareScheme: process.env.MIDDLEWARE_SCHEME,
-    middlewareHost: process.env.MIDDLEWARE_HOST,
-    middlewarePort: process.env.MIDDLEWARE_PORT,
+    middlewareScheme: process.env.MIDDLEWARE_SCHEME || "http",
+    middlewareHost: process.env.MIDDLEWARE_HOST || "localhost",
+    middlewarePort: process.env.MIDDLEWARE_PORT || "8080",
   },
   experimental: { esmExternals: true },
   ...nextConfig
 });
+
+//export MIDDLEWARE_SCHEME=http
