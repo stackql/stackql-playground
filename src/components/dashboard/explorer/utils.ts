9983aac990a3d4ba0f6e3464b6b174b3649d6962
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-17 00:15:20 +1100

    minor fixes;

--- src/components/dashboard/explorer/utils.ts
+++ src/components/dashboard/explorer/utils.ts
@@ -1,26 +1,5 @@
 import { ItemLevel, RenderTree } from "../../../types";
 
-const getUpdateIndexes = (roots: RenderTree[], node: RenderTree) => {
-  const updatePath = node.path;
-  const updateIndexes: {
-    provider?: number;
-    service?: number;
-  } = { provider: undefined, service: undefined };
-  const providerNodeIndex = roots?.findIndex((root) =>
-    updatePath.startsWith(root.name)
-  );
-  if (providerNodeIndex) {
-    updateIndexes.provider = providerNodeIndex;
-  }
-  if (node.level === ItemLevel.service) {
-    const serviceIndex = roots?.[providerNodeIndex!].children?.findIndex(
-      (service) => updatePath === service.path
-    );
-    updateIndexes.service = serviceIndex;
-  }
-  return updateIndexes;
-};
-
 export const populateItemTree = (
   root: RenderTree,
   updateNode: RenderTree,
@@ -39,7 +18,6 @@ export const populateItemTree = (
   ) {
     rootClone.children?.map((service) => {
       if (service.path === updateNode.path) {
-        console.log("updating service %o", service);
         service.children = newChildren;
       }
       return service;
