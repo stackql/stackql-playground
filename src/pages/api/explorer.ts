2fda860902e0f5d1c2751c6d8cdf14ae8830928c
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-16 14:55:25 +1100

    fix: remove the file change not in the branch scope

--- src/pages/api/explorer.ts
+++ src/pages/api/explorer.ts
@@ -13,45 +13,43 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const url = middlewareUrl;
-
   const providers = (await getDataFromResponse<Provider[]>(
     providersUrl(url)
   )) as Provider[];
   console.log("providers are %o", providers);
-  // console.log("providers are %o", providers);
-  // const entityTreePromises = providers.map(
-  //   async (provider: { name: string }, index: { toString: () => any }) => {
-  //     const servicesUrl = getServiceUrl(url, provider.name);
-  //     const services = (await getDataFromResponse<Service[]>(
-  //       servicesUrl
-  //     )) as Service[];
+  const entityTreePromises = providers.map(
+    async (provider: { name: string }, index: { toString: () => any }) => {
+      const servicesUrl = getServiceUrl(url, provider.name);
+      const services = (await getDataFromResponse<Service[]>(
+        servicesUrl
+      )) as Service[];
 
-  //     const serviceWithResource: any[] = [];
-  //     for (const service of services) {
-  //       const resourceUrl = getResourceUrl(url, provider.name, service.name);
-  //       const resources = (await getDataFromResponse<Resource[]>(
-  //         resourceUrl
-  //       )) as Resource[];
-  //       serviceWithResource.push({
-  //         ...service,
-  //         level: 1,
-  //         children: resources.map((resource: { name: any }) => ({
-  //           ...resource,
-  //           level: 2,
-  //           path: `${provider.name}.${service.name}.${resource.name}`,
-  //         })),
-  //       });
-  //     }
-  //     const providerWithService = {
-  //       ...provider,
-  //       level: 0,
-  //       children: serviceWithResource,
-  //       id: index.toString(),
-  //     };
-  //     return providerWithService;
-  //   }
-  // );
+      const serviceWithResource: any[] = [];
+      for (const service of services) {
+        const resourceUrl = getResourceUrl(url, provider.name, service.name);
+        const resources = (await getDataFromResponse<Resource[]>(
+          resourceUrl
+        )) as Resource[];
+        serviceWithResource.push({
+          ...service,
+          level: 1,
+          children: resources.map((resource: { name: any }) => ({
+            ...resource,
+            level: 2,
+            path: `${provider.name}.${service.name}.${resource.name}`,
+          })),
+        });
+      }
+      const providerWithService = {
+        ...provider,
+        level: 0,
+        children: serviceWithResource,
+        id: index.toString(),
+      };
+      return providerWithService;
+    }
+  );
 
-  // const entityTree = (await Promise.all(entityTreePromises)) as RenderTree[];
-  res.status(200).json(providers);
+  const entityTree = (await Promise.all(entityTreePromises)) as RenderTree[];
+  res.status(200).json(entityTree);
 }
