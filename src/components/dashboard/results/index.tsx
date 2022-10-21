ba51cddd505b875edadb7ac94f05fa5e75bc7bde
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-22 01:08:19 +1100

    fixed query pane expand issues when query is long

--- src/components/dashboard/results/index.tsx
+++ src/components/dashboard/results/index.tsx
@@ -58,7 +58,7 @@ export default function ResultsPanel() {
   return (
     <div className="flex h-full flex-col">
       <h2 className="panel-title pl-2 bg-gray-100 border-bottom">Results</h2>
-      <div className="w-full h-[90%] rounded-sm bg-gray-50 p-4 shadow-sm overflow-auto">
+      <div className="w-full h-[90%] rounded-sm bg-gray-50 px-4 shadow-sm">
         {queryResults && RenderQueryResult(queryResults)}
       </div>
     </div>
