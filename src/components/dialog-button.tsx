9016359993f7ed25a8952221bf277203ad0aab0a
Yuncheng Yang <yunchengyang@Yunchengs-MacBook-Pro.local>
2022-10-28 21:53:38 +1100

    added server url and frontend, refactored the fetch method

---
+++ src/components/dialog-button.tsx
@@ -0,0 +1,74 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { ReactElement, useState } from "react";
+
+export interface ButtonDialogFormProps {
+  open?: boolean;
+  title: string;
+  textField?: ReactElement | ReactElement[];
+  subText?: string;
+  buttonText?: string;
+  onSubmit?: () => void;
+}
+
+export const DialogButton = ({
+  title,
+  subText,
+  buttonText,
+  textField,
+  onSubmit,
+}: ButtonDialogFormProps) => {
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    onSubmit && onSubmit();
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        onClick={handleClickOpen}
+        className="text-primary border border-primary bg-white"
+      >
+        {buttonText}
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{subText}</DialogContentText>
+          {textField}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            className="text-primary text-center border-primary"
+            variant="outlined"
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="text-primary text-center border-primary"
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
---
+++ src/components/layout/add-url-button.tsx
@@ -0,0 +1,44 @@
+import { Input, TextField } from "@mui/material";
+import { useState } from "react";
+import { useQueryContext } from "../../contexts/queryContext/useQueryContext";
+import { primaryColorHex } from "../../types";
+import { DialogButton } from "../dialog-button";
+
+export const AddUrlButton = () => {
+  const { setServerUrl } = useQueryContext();
+  const [inputText, setInputText] = useState<string>("");
+  const title = "Connect to StackQL Server";
+  const handleSubmit = () => {
+    setServerUrl(inputText);
+  };
+  const textField = (
+    <Input
+      autoFocus
+      margin="dense"
+      id="name"
+      placeholder="https://"
+      type="url"
+      fullWidth
+      onChange={(event) => {
+        setInputText(event.target.value);
+      }}
+      sx={{
+        ":before": { borderBottomColor: primaryColorHex },
+        // underline when selected
+        ":after": { borderBottomColor: primaryColorHex },
+      }}
+    />
+  );
+
+  const onSubmit = () => {
+    handleSubmit();
+  };
+  return (
+    <DialogButton
+      title={title}
+      buttonText={title}
+      textField={textField}
+      onSubmit={onSubmit}
+    />
+  );
+};
